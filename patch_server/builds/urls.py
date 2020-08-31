8eea84fdd6a1f5531bc60c59bdaacbd25ff4b759
maxnoller <max.noller1@gmail.com>
2020-08-31 15:41:22 +0200

    Added newer version availible url

--- patch_server/builds/urls.py
+++ patch_server/builds/urls.py
@@ -3,4 +3,5 @@ from .views import get_new_files, newer_version_availible
 
 urlpatterns = [
     path("compare/", get_new_files),
+    path("check_newest_version/", newer_version_availible),
 ]
--- patch_server/builds/views.py
+++ patch_server/builds/views.py
@@ -1,6 +1,6 @@
 import decimal
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from .models import Build
 
