f7466c8a0112f99a28d84457c17c06a2a155e8a3
maxnoller <max.noller1@gmail.com>
2020-09-21 16:18:00 +0200

    changed settings to be able to use production env

--- patch_server/patch_server/wsgi.py
+++ patch_server/patch_server/wsgi.py
@@ -1,16 +1,15 @@
 """
-WSGI config for patch_server project.
-
+WSGI config for azuresite project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'patch_server.settings')
-
-application = get_wsgi_application()
+if os.environ.get('DJANGO_ENV') == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azuresite.production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azuresite.settings')    
